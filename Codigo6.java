bba647db3a7c9a071e04eb675bf817b001cf1b93
Paulina-Ensastiga <celypau@gmail.com>
2023-07-07 13:15:38 -0600

    4 5 6

---
+++ Codigo6.java
@@ -0,0 +1,40 @@
+package org.generation.cd5;
+
+import java.util.Scanner;
+
+public class Codigo6 {
+
+	public static void main(String[] args) {
+			  
+		    int[] n = new int[20];
+
+		    for (int i = 0; i < 20; i++) {
+		      n[i] = (int)(Math.random() * 381) + 20;
+		      System.out.print(n[i] + " ");
+		    }
+		    
+		    System.out.print("\n¿Qué números quiere resaltar? ");
+		    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+		    
+		    Scanner sc = new Scanner(System.in);
+		    int opcion = sc.nextInt();
+
+		    for (int i = 0; i < 20; i++) {
+		      n[i] = (int)(Math.random() * 381) + 20;
+		      System.out.print(n[i] + " ");
+		    }
+		   
+		    int multiplo = (opcion == 1)?  5 : 7;
+
+		   for(int e : n){
+		      if (e % multiplo == 0) {
+		        System.out.print("[" + e + "] ");
+		      }else{
+		        System.out.print(e + " ");
+		      }
+		    }
+		  
+		}
+
+	}
+
